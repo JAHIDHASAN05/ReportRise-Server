6d66f9363fd4355e56ffece83f16033c401ca659
JAHID HASAN <developerjahid@gmail.com>
2024-01-27 21:56:48 +0600

    newsfilter by category id

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ const express= require('express')
 const app = express()
 
 const port= 5000;
+const category= require('./Data/category.json')
+const news= require('./Data/news.json')
+
+const cors=require('cors')
+app.use(cors())
 
 app.get('/', (req, res)=>{
    res.send('REport Rise is live')
 })
+
+app.get('/category', (req, res)=>{
+    res.send(category)
+})
+
+app.get('/category/:id', (req, res)=>{
+    const id =req.params.id
+    const categoryNews= news.filter(n=> n.category_id=== id)
+    res.send(categoryNews)
+})
+
 app.listen(port, (req, res)=>{
     console.log(`runnng at ${port} `)
 })
